fd55ca1886e5594cdc0ffacd506b62457c161737
mrmoneyc <mmc@mrmoneyc.org>
2017-07-01 02:04:29 +0800

    feat: initial commit, first work version

---
+++ main.go
@@ -0,0 +1,248 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"sort"
+	"strings"
+	"sync"
+
+	"github.com/mrjones/oauth"
+)
+
+const (
+	consumerKey       string = ""
+	consumerSecret    string = ""
+	requestTokenURL   string = "https://api.twitter.com/oauth/request_token"
+	authorizeTokenURL string = "https://api.twitter.com/oauth/authorize"
+	accessTokenURL    string = "https://api.twitter.com/oauth/access_token"
+	apiBase           string = "https://api.twitter.com/1.1/"
+	apiFavoriteList   string = apiBase + "favorites/list.json?count=201"
+)
+
+func getConfig() (string, map[string]string, error) {
+	cfgFolder := os.Getenv("HOME")
+
+	if cfgFolder == "" && runtime.GOOS == "windows" {
+		cfgFolder = os.Getenv("APPDATA")
+
+		if cfgFolder == "" {
+			cfgFolder = filepath.Join(os.Getenv("USERPROFILE"), "Application Data", "TwitterFavoritePics")
+		}
+
+		cfgFolder = filepath.Join(cfgFolder, "TwitterFavoritePics")
+	} else {
+		cfgFolder = filepath.Join(cfgFolder, ".config", "TwitterFavoritePics")
+	}
+
+	if err := os.MkdirAll(cfgFolder, 0700); err != nil {
+		return "", nil, err
+	}
+
+	cfgFile := filepath.Join(cfgFolder, "settings.json")
+
+	cfg := map[string]string{}
+
+	buf, err := ioutil.ReadFile(cfgFile)
+	if err != nil && !os.IsNotExist(err) {
+		return "", nil, err
+	}
+
+	if err != nil {
+		cfg["ConsumerKey"] = consumerKey
+		cfg["ConsumerSecret"] = consumerSecret
+		cfg["FilterAccount"] = ""
+	} else {
+		err = json.Unmarshal(buf, &cfg)
+		if err != nil {
+			return "", nil, fmt.Errorf("Could not unmarshal %v: %v", cfgFile, err)
+		}
+	}
+
+	return cfgFile, cfg, nil
+}
+
+func writeConfig(cfg map[string]string, cfgFile string) (bool, error) {
+	buf, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return false, err
+	}
+
+	err = ioutil.WriteFile(cfgFile, buf, 0700)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func getAuthorizeToken(c *oauth.Consumer, cfg map[string]string) (*oauth.AccessToken, error) {
+	accessToken, foundToken := cfg["AccessToken"]
+	accessSecret, foundSecret := cfg["AccessSecret"]
+
+	var authorizeToken *oauth.AccessToken
+
+	if foundToken && foundSecret {
+		authorizeToken = &oauth.AccessToken{Token: accessToken, Secret: accessSecret}
+	} else {
+		reqToken, url, err := c.GetRequestTokenAndUrl("")
+		if err != nil {
+			return nil, err
+		}
+
+		browser := "xdg-open"
+		args := []string{url}
+
+		if runtime.GOOS == "windows" {
+			browser = "rundll32.exe"
+			args = []string{"url.dll,FileProtocolHandler", url}
+		} else if runtime.GOOS == "darwin" {
+			browser = "open"
+		}
+
+		fmt.Println("(1) Go to: " + url)
+		fmt.Println("(2) Grant access, you should get back a verification code.")
+
+		browser, err = exec.LookPath(browser)
+		if err == nil {
+			cmd := exec.Command(browser, args...)
+			cmd.Stderr = os.Stderr
+			err = cmd.Start()
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		fmt.Print("(3) Enter that verification code here: ")
+		verificationCode := ""
+		fmt.Scanln(&verificationCode)
+
+		authorizeToken, err = c.AuthorizeToken(reqToken, verificationCode)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return authorizeToken, nil
+}
+
+func downloadMedia(wg *sync.WaitGroup, url string, fileName string, screenName string) {
+	defer wg.Done()
+	fmt.Printf("Get %v\n", fileName)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	currentPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	downloadPath := filepath.Join(currentPath, "downloads", screenName)
+	if err := os.MkdirAll(downloadPath, 0755); err != nil {
+		log.Fatal(err)
+	}
+
+	f, err := os.Create(filepath.Join(downloadPath, fileName))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = io.Copy(f, resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	f.Close()
+}
+
+func main() {
+	cfgFile, cfg, err := getConfig()
+	if err != nil {
+		log.Fatal("Get configuration file failed: ", err)
+	}
+
+	var filterAccount []string
+	if len(cfg["FilterAccount"]) > 0 {
+		filterAccount = strings.Split(cfg["FilterAccount"], ",")
+		sort.Strings(filterAccount)
+	}
+
+	c := oauth.NewConsumer(
+		cfg["ConsumerKey"],
+		cfg["ConsumerSecret"],
+		oauth.ServiceProvider{
+			RequestTokenUrl:   requestTokenURL,
+			AuthorizeTokenUrl: authorizeTokenURL,
+			AccessTokenUrl:    accessTokenURL,
+		},
+	)
+
+	authorizeToken, err := getAuthorizeToken(c, cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg["AccessToken"] = authorizeToken.Token
+	cfg["AccessSecret"] = authorizeToken.Secret
+	_, err = writeConfig(cfg, cfgFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := c.MakeHttpClient(authorizeToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Get(apiFavoriteList)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var fav FavoriteList
+
+	if err := json.Unmarshal([]byte(body), &fav); err != nil {
+		log.Fatal(err)
+	}
+
+	var wg sync.WaitGroup
+
+	for _, v := range fav {
+		i := sort.SearchStrings(filterAccount, v.User.ScreenName)
+
+		if i < len(filterAccount) && filterAccount[i] == v.User.ScreenName {
+			for _, val := range v.Entities.Media {
+				// fmt.Printf("[%v] media_url: %v\n", k, val.MediaURL)
+				largeMediaURL := val.MediaURL + ":large"
+				sl := strings.Split(val.MediaURL, "/")
+				fileName := sl[len(sl)-1]
+				// fmt.Printf("%v -> %v, %v\n", k, largeMediaURL, fileName)
+
+				wg.Add(1)
+				go downloadMedia(&wg, largeMediaURL, fileName, v.User.ScreenName)
+			}
+		}
+	}
+
+	wg.Wait()
+	fmt.Println("Done")
+}
---
+++ response_struct.go
@@ -0,0 +1,106 @@
+package main
+
+// FavoriteList is mapping to user favorited tweet list
+type FavoriteList []struct {
+	Coordinates        interface{} `json:"coordinates"`
+	Truncated          bool        `json:"truncated"`
+	Favorited          bool        `json:"favorited"`
+	CreatedAt          string      `json:"created_at"`
+	IDStr              string      `json:"id_str"`
+	InReplyToUserIDStr interface{} `json:"in_reply_to_user_id_str"`
+	Entities           struct {
+		Urls         []interface{} `json:"urls"`
+		HashTags     []interface{} `json:"hashtags"`
+		UserMentions []interface{} `json:"user_mentions"`
+		Media        []struct {
+			ID            int    `json:"id"`
+			IDStr         string `json:"id_str"`
+			Indice        []int  `json:"indices"`
+			MediaURL      string `json:"media_url"`
+			MediaURLHTTPS string `json:"media_url_https"`
+			URL           string `json:"url"`
+			DisplayURL    string `json:"display_url"`
+			ExpandedURL   string `json:"expanded_url"`
+			Type          string `json:"type"`
+			Sizes         struct {
+				Medium struct {
+					W      int    `json:"w"`
+					H      int    `json:"h"`
+					Resize string `json:"resize"`
+				} `json:"medium"`
+				Thumb struct {
+					W      int    `json:"w"`
+					H      int    `json:"h"`
+					Resize string `json:"resize"`
+				} `json:"thumb"`
+				Small struct {
+					W      int    `json:"w"`
+					H      int    `json:"h"`
+					Resize string `json:"resize"`
+				} `json:"small"`
+				Large struct {
+					W      int    `json:"w"`
+					H      int    `json:"h"`
+					Resize string `json:"resize"`
+				} `json:"large"`
+			} `json:"sizes"`
+		} `json:"media"`
+	} `json:"entities"`
+	Text                 string      `json:"text"`
+	Contributors         interface{} `json:"contributors"`
+	ID                   int         `json:"id"`
+	RetweetCount         int         `json:"retweet_count"`
+	InReplyToStatusIDStr interface{} `json:"in_reply_to_status_id_str"`
+	Geo                  interface{} `json:"geo"`
+	Retweeted            bool        `json:"retweeted"`
+	InReplyToUserID      interface{} `json:"in_reply_to_user_id"`
+	InReplyToScreenName  interface{} `json:"in_reply_to_screen_name"`
+	Source               string      `json:"source"`
+	User                 struct {
+		ProfileSidebarFillColor   string `json:"profile_sidebar_fill_color"`
+		ProfileBackgroundTile     bool   `json:"profile_background_tile"`
+		ProfileSidebarBorderColor string `json:"profile_sidebar_border_color"`
+		Name                      string `json:"name"`
+		ProfileImageURL           string `json:"profile_image_url"`
+		Location                  string `json:"location"`
+		CreatedAt                 string `json:"created_at"`
+		FollowRequestSent         bool   `json:"follow_request_sent"`
+		IsTranslator              bool   `json:"is_translator"`
+		IDStr                     string `json:"id_str"`
+		ProfileLinkColor          string `json:"profile_link_color"`
+		Entities                  struct {
+			Description struct {
+				Urls []interface{} `json:"urls"`
+			} `json:"description"`
+		} `json:"entities"`
+		FavoritesCount                 int    `json:"favourites_count"`
+		URL                            string `json:"url"`
+		DefaultProfile                 bool   `json:"default_profile"`
+		ContributorsEnabled            bool   `json:"contributors_enabled"`
+		ProfileImageURLHTTPS           string `json:"profile_image_url_https"`
+		UTCOffset                      int    `json:"utc_offset"`
+		ID                             int    `json:"id"`
+		ListedCount                    int    `json:"listed_count"`
+		ProfileUseBackgroundImage      bool   `json:"profile_use_background_image"`
+		FollowersCount                 int    `json:"followers_count"`
+		Protected                      bool   `json:"protected"`
+		ProfileTextColor               string `json:"profile_text_color"`
+		Lang                           string `json:"lang"`
+		ProfileBackgroundColor         string `json:"profile_background_color"`
+		TimeZone                       string `json:"time_zone"`
+		Verified                       bool   `json:"verified"`
+		ProfileBackgroundImageURLHTTPS string `json:"profile_background_image_url_https"`
+		Description                    string `json:"description"`
+		GeoEnabled                     bool   `json:"geo_enabled"`
+		Notifications                  bool   `json:"notifications"`
+		DefaultProfileImage            bool   `json:"default_profile_image"`
+		FriendsCount                   int    `json:"friends_count"`
+		ProfileBackgroundImageURL      string `json:"profile_background_image_url"`
+		StatusesCount                  int    `json:"statuses_count"`
+		Following                      bool   `json:"following"`
+		ScreenName                     string `json:"screen_name"`
+		ShowAllInlineMedia             bool   `json:"show_all_inline_media"`
+	} `json:"user"`
+	Place             interface{} `json:"place"`
+	InReplyToStatusID interface{} `json:"in_reply_to_status_id"`
+}
